72050c37e9c1b6c509da4308107be5cef6a5f0c6
a1dram <aldram54@gmail.com>
2026-03-13 20:53:17 +0300

    read_seq: readSequences() maket created

--- muraviev.alexander/S1/read_seq.cpp
+++ muraviev.alexander/S1/read_seq.cpp
@@ -13,3 +13,43 @@ bool muraviev::isLineFinished(std::istream& in)
   int ch = in.peek();
   return !in || ch == '\n' || ch == EOF;
 }
+
+void muraviev::skipLineSpaces(std::istream& in)
+{
+  while (in && isLineSpace(in.peek())) {
+    in.get();
+  }
+}
+
+muraviev::List< muraviev::namedSequence > muraviev::readSequences(std::istream& in)
+{
+  List< namedSequence > sequences = {};
+
+  while (in) {
+    std::string name = "";
+    if (!(in >> name)) {
+      break;
+    }
+
+    namedSequence seq = {};
+    seq.name = name;
+
+    while (in) {
+      skipLineSpaces(in);
+      if (isLineFinished(in)) {
+        break;
+      }
+
+      size_t value = 0;
+      in >> value;
+      if (!in) {
+        break;
+      }
+      pushNumber(seq.numbers, value);
+    }
+
+    pushSequence(sequences, seq);
+  }
+
+  return sequences;
+}
