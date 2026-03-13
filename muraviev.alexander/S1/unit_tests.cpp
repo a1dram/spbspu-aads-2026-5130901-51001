e199ecf9b2c3072221be66a3ed4b2da37809a406
a1dram <aldram54@gmail.com>
2026-03-14 00:50:06 +0300

    unit_tests.cpp is fully recreated to check

--- muraviev.alexander/S1/unit_tests.cpp
+++ muraviev.alexander/S1/unit_tests.cpp
@@ -1,5 +1,3 @@
-#ifdef RUN_UNIT_TESTS
-
 #define BOOST_TEST_MODULE S1
 #include <boost/test/included/unit_test.hpp>
 
@@ -10,199 +8,245 @@
 
 using namespace muraviev;
 
-BOOST_AUTO_TEST_CASE(test_list_empty_after_default_constructor)
+BOOST_AUTO_TEST_CASE(test_list_is_empty)
 {
-  List< int > lst;
-  BOOST_TEST(lst.empty());
+  List< int > l;
+  BOOST_TEST(l.empty());
 }
 
-BOOST_AUTO_TEST_CASE(test_list_not_empty_after_push_front)
+BOOST_AUTO_TEST_CASE(test_list_push_front)
 {
-  List< int > lst;
-  lst.pushFront(5);
-  BOOST_TEST(!lst.empty());
+  List< int > l;
+  l.pushFront(10);
+  BOOST_TEST(!l.empty());
+  BOOST_TEST(*l.begin() == 10);
 }
 
-BOOST_AUTO_TEST_CASE(test_list_push_front_order)
+BOOST_AUTO_TEST_CASE(test_list_insert)
 {
-  List< int > lst;
-  lst.pushFront(1);
-  lst.pushFront(2);
-  lst.pushFront(3);
+  List< int > l;
+  l.pushFront(1);
+  l.insert(l.last(), 2);
 
-  int expected[3] = {3, 2, 1};
-  size_t i = 0;
-  for (List< int >::c_iter it = lst.begin(); it != lst.end(); ++it) {
-    BOOST_TEST(*it == expected[i]);
-    ++i;
-  }
-  BOOST_TEST(i == 3);
-}
-
-BOOST_AUTO_TEST_CASE(test_list_insert_after_last)
-{
-  List< int > lst;
-  lst.pushFront(1);
-  lst.insert(lst.last(), 2);
-  lst.insert(lst.last(), 3);
-
-  int expected[3] = {1, 2, 3};
-  size_t i = 0;
-  for (List< int >::c_iter it = lst.begin(); it != lst.end(); ++it) {
-    BOOST_TEST(*it == expected[i]);
-    ++i;
-  }
-  BOOST_TEST(i == 3);
+  List< int >::iter it = l.begin();
+  BOOST_TEST(*it == 1);
+  ++it;
+  BOOST_TEST(*it == 2);
 }
 
 BOOST_AUTO_TEST_CASE(test_list_pop_front)
 {
-  List< int > lst;
-  lst.pushFront(3);
-  lst.pushFront(2);
-  lst.pushFront(1);
+  List< int > l;
+  l.pushFront(2);
+  l.pushFront(1);
+  l.popFront();
 
-  lst.popFront();
-
-  int expected[2] = {2, 3};
-  size_t i = 0;
-  for (List< int >::c_iter it = lst.begin(); it != lst.end(); ++it) {
-    BOOST_TEST(*it == expected[i]);
-    ++i;
-  }
-  BOOST_TEST(i == 2);
+  BOOST_TEST(*l.begin() == 2);
 }
 
 BOOST_AUTO_TEST_CASE(test_list_pop_back)
 {
-  List< int > lst;
-  lst.pushFront(3);
-  lst.pushFront(2);
-  lst.pushFront(1);
+  List< int > l;
+  l.pushFront(2);
+  l.pushFront(1);
+  l.popBack();
 
-  lst.popBack();
-
-  int expected[2] = {1, 2};
-  size_t i = 0;
-  for (List< int >::c_iter it = lst.begin(); it != lst.end(); ++it) {
-    BOOST_TEST(*it == expected[i]);
-    ++i;
-  }
-  BOOST_TEST(i == 2);
+  List< int >::iter it = l.begin();
+  BOOST_TEST(*it == 1);
+  ++it;
+  BOOST_TEST(it == l.end());
 }
 
-BOOST_AUTO_TEST_CASE(test_list_erase_middle)
+BOOST_AUTO_TEST_CASE(test_list_erase)
 {
-  List< int > lst;
-  lst.pushFront(3);
-  lst.pushFront(2);
-  lst.pushFront(1);
+  List< int > l;
+  l.pushFront(3);
+  l.pushFront(2);
+  l.pushFront(1);
 
-  List< int >::iter it = lst.begin();
+  List< int >::iter it = l.begin();
   ++it;
-  lst.erase(it);
+  l.erase(it);
 
-  int expected[2] = {1, 3};
-  size_t i = 0;
-  for (List< int >::c_iter jt = lst.begin(); jt != lst.end(); ++jt) {
-    BOOST_TEST(*jt == expected[i]);
-    ++i;
-  }
-  BOOST_TEST(i == 2);
+  it = l.begin();
+  BOOST_TEST(*it == 1);
+  ++it;
+  BOOST_TEST(*it == 3);
 }
 
 BOOST_AUTO_TEST_CASE(test_list_clear)
 {
-  List< int > lst;
-  lst.pushFront(1);
-  lst.pushFront(2);
-  lst.pushFront(3);
-
-  lst.clear();
-
-  BOOST_TEST(lst.empty());
+  List< int > l;
+  l.pushFront(1);
+  l.clear();
+  BOOST_TEST(l.empty());
 }
 
 BOOST_AUTO_TEST_CASE(test_list_copy_constructor)
 {
-  List< int > a;
-  a.pushFront(3);
-  a.pushFront(2);
-  a.pushFront(1);
+  List< int > l1;
+  l1.pushFront(1);
+  l1.pushFront(2);
 
-  List< int > b(a);
-
-  List< int >::c_iter it1 = a.begin();
-  List< int >::c_iter it2 = b.begin();
-  while (it1 != a.end() && it2 != b.end()) {
-    BOOST_TEST(*it1 == *it2);
-    ++it1;
-    ++it2;
-  }
-
-  int count1 = 0;
-  for (List< int >::c_iter it = a.begin(); it != a.end(); ++it) {
-    ++count1;
-  }
-
-  int count2 = 0;
-  for (List< int >::c_iter it = b.begin(); it != b.end(); ++it) {
-    ++count2;
-  }
-
-  BOOST_TEST(count1 == count2);
+  List< int > l2(l1);
+  List< int >::iter it = l2.begin();
+  BOOST_TEST(*it == 2);
+  ++it;
+  BOOST_TEST(*it == 1);
 }
 
-BOOST_AUTO_TEST_CASE(test_list_iterators_on_loop)
+BOOST_AUTO_TEST_CASE(test_list_iterators)
 {
-  List< int > lst;
-  lst.pushFront(3);
-  lst.pushFront(2);
-  lst.pushFront(1);
-
-  int expected[3] = {1, 2, 3};
-  size_t i = 0;
-  for (List< int >::iter it = lst.begin(); it != lst.end(); ++it) {
-    BOOST_TEST(*it == expected[i]);
-    ++i;
+  List< int > l;
+  l.pushFront(1);
+  l.pushFront(2);
+  int count = 0;
+  for (List< int >::iter it = l.begin(); it != l.end(); ++it) {
+    count++;
   }
-  BOOST_TEST(i == 3);
+  BOOST_TEST(count == 2);
 }
 
-BOOST_AUTO_TEST_CASE(test_get_list_size)
+BOOST_AUTO_TEST_CASE(test_print_get_list_size)
 {
   List< size_t > nums;
-  nums.pushFront(3);
-  nums.pushFront(2);
-  nums.pushFront(1);
-
-  size_t size = getListSize(nums);
-  BOOST_TEST(size == 3);
+  nums.pushFront(99);
+  nums.pushFront(88);
+  BOOST_TEST(getListSize(nums) == 2);
 }
 
-BOOST_AUTO_TEST_CASE(test_has_index)
+BOOST_AUTO_TEST_CASE(test_print_has_index)
 {
   List< size_t > nums;
-  nums.pushFront(3);
-  nums.pushFront(2);
-  nums.pushFront(1);
-
+  nums.pushFront(5);
+  nums.pushFront(4);
   BOOST_TEST(hasIndex(nums, 0));
   BOOST_TEST(hasIndex(nums, 1));
-  BOOST_TEST(hasIndex(nums, 2));
-  BOOST_TEST(!hasIndex(nums, 3));
+  BOOST_TEST(!hasIndex(nums, 2));
 }
 
-BOOST_AUTO_TEST_CASE(test_get_value_at)
+BOOST_AUTO_TEST_CASE(test_print_get_value_at)
 {
   List< size_t > nums;
-  nums.pushFront(3);
-  nums.pushFront(2);
-  nums.pushFront(1);
-
-  BOOST_TEST(getValueAt(nums, 0) == 1);
-  BOOST_TEST(getValueAt(nums, 1) == 2);
-  BOOST_TEST(getValueAt(nums, 2) == 3);
+  nums.pushFront(42);
+  nums.pushFront(24);
+  BOOST_TEST(getValueAt(nums, 0) == 24);
+  BOOST_TEST(getValueAt(nums, 1) == 42);
 }
 
-#endif
+BOOST_AUTO_TEST_CASE(test_new_list_is_empty)
+{
+  List< int > l;
+  BOOST_TEST(l.empty());
+}
+
+BOOST_AUTO_TEST_CASE(test_push_and_clear)
+{
+  List< int > l;
+  l.pushFront(10);
+  l.pushFront(20);
+  BOOST_TEST(!l.empty());
+
+  l.clear();
+  BOOST_TEST(l.empty());
+}
+
+BOOST_AUTO_TEST_CASE(test_iterator_prefix_increment)
+{
+  List< int > l;
+  l.pushFront(30);
+  l.pushFront(20);
+  l.pushFront(10);
+
+  List< int >::iter it = l.begin();
+  BOOST_TEST(*it == 10);
+  ++it;
+  BOOST_TEST(*it == 20);
+  ++it;
+  BOOST_TEST(*it == 30);
+  ++it;
+
+  BOOST_TEST(it == l.end());
+}
+
+BOOST_AUTO_TEST_CASE(test_copy_assignment_operator)
+{
+  List< int > l1;
+  l1.pushFront(5);
+  l1.pushFront(15);
+
+  List< int > l2;
+  l2 = l1;
+
+  List< int >::iter it = l2.begin();
+  BOOST_TEST(*it == 15);
+  ++it;
+  BOOST_TEST(*it == 5);
+}
+
+BOOST_AUTO_TEST_CASE(test_pop_front_till_empty)
+{
+  List< int > l;
+  l.pushFront(100);
+  l.pushFront(200);
+
+  l.popFront();
+  BOOST_TEST(*l.begin() == 100);
+
+  l.popFront();
+  BOOST_TEST(l.empty());
+}
+
+BOOST_AUTO_TEST_CASE(test_pop_back_behavior)
+{
+  List< int > l;
+  l.pushFront(3);
+  l.pushFront(2);
+  l.pushFront(1);
+
+  l.popBack();
+
+  List< int >::iter it = l.begin();
+  BOOST_TEST(*it == 1);
+  ++it;
+  BOOST_TEST(*it == 2);
+  ++it;
+  BOOST_TEST(it == l.end());
+}
+
+BOOST_AUTO_TEST_CASE(test_print_seq_get_max_size)
+{
+  List< namedSequence > seqs;
+
+  namedSequence s1;
+  s1.name = "seq1";
+  s1.numbers.pushFront(1);
+
+  namedSequence s2;
+  s2.name = "seq2";
+  s2.numbers.pushFront(3);
+  s2.numbers.pushFront(2);
+
+  seqs.pushFront(s2);
+  seqs.pushFront(s1);
+
+  BOOST_TEST(getMaxSize(seqs) == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_print_seq_get_row_sum)
+{
+  List< namedSequence > seqs;
+
+  namedSequence s1;
+  s1.name = "s1";
+  s1.numbers.pushFront(10);
+
+  namedSequence s2;
+  s2.name = "s2";
+  s2.numbers.pushFront(25);
+
+  seqs.pushFront(s2);
+  seqs.pushFront(s1);
+
+  BOOST_TEST(getRowSum(seqs, 0) == 35);
+}
