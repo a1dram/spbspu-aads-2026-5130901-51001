66ab9f6b120f1b823b556a79e63af9936b943e5a
a1dram <aldram54@gmail.com>
2026-05-23 22:29:01 +0300

    added node + search to RBTree

--- muraviev.alexander/F0/rb-tree.hpp
+++ muraviev.alexander/F0/rb-tree.hpp
@@ -1,6 +1,9 @@
 #ifndef RB_TREE_HPP
 #define RB_TREE_HPP
 
+#include <cstddef>
+#include <stdexcept>
+
 namespace muraviev
 {
   template< class T >
@@ -16,6 +19,55 @@ namespace muraviev
   {
     RED,
     BLACK
+  };
+
+  template< class Key, class Value >
+  struct RBNode
+  {
+    Key key;
+    Value value;
+    RBNode* parent;
+    RBNode* left;
+    RBNode* right;
+    RBColor color;
+
+    RBNode(const Key& nodeKey, const Value& nodeValue):
+      key(nodeKey),
+      value(nodeValue),
+      parent(0),
+      left(0),
+      right(0),
+      color(RED)
+    {}
+  };
+
+  template< class Key, class Value, class Compare >
+  class RBTree
+  {
+  public:
+    RBTree(): root_(0), size_(0) {}
+    bool empty() const { return size_ == 0; }
+    size_t size() const { return size_; }
+    bool contains(const Key& key) const { return findNode(key) != 0; }
+  private:
+    RBNode< Key, Value >* root_;
+    size_t size_;
+    Compare compare_;
+
+    RBNode< Key, Value >* findNode(const Key& key) const
+    {
+      RBNode< Key, Value >* current = root_;
+      while (current != 0) {
+        if (compare_(key, current->key)) {
+          current = current->left;
+        } else if (compare_(current->key, key)) {
+          current = current->right;
+        } else {
+          return current;
+        }
+      }
+      return 0;
+    }
   };
 }
 
