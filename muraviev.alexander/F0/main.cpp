a6b0fd5586c05ad265c82667c7863579a3fb5585
a1dram <aldram54@gmail.com>
2026-05-23 21:57:49 +0300

    F0 initial commit: simple main

---
+++ muraviev.alexander/F0/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    if (!line.empty()) {
+      std::cout << "<INVALID COMMAND>\n";
+    }
+  }
+  return 0;
+}
